9d2bac4b728d63137b34ce56b85150299a49b80e
Rafael Teles <rafaeltelesportela@gmail.com>
2022-08-11 16:43:09 -0300

    commit full course

---
+++ Estudos_JS_Udemy/JavaScript/CursoJavaScript/Section 3/challenge_area.js
@@ -0,0 +1,10 @@
+//PI * (raio * raio)
+//PI = Math.PI
+let raio = 10;
+const PI = 3.141592;
+let area = PI * (raio * raio);
+
+console.log("Valor da Área: " + area + "m2");
+
+area = Math.PI * raio * raio;
+console.log("Valor da Área: " + area + "m2");
---
+++ Estudos_JS_Udemy/JavaScript/CursoJavaScript/Section 3/comment.js
@@ -0,0 +1,11 @@
+//console.log é usado para exibir algo na tela do computador!
+console.log('Step #1');
+console.log('Step #2');
+console.log('Step #3');
+
+
+/*              | 
+ * Comentário em| Multiline
+ * várias linhas| comment in
+ * no código.   | code.
+ */
