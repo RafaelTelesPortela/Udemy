2814c635fc627453f645d047bafec5db9ac94d8b
Rafael Teles <rafaeltelesportela@gmail.com>
2022-08-29 10:52:22 -0300

    Commint full course

---
+++ Estudos_JS_Udemy/JavaScript/CursoJavaScript/Section 4/Exercicios-JS/Fundamentos/aritimeticos.js
@@ -0,0 +1,22 @@
+const [ a, b , c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicao = a * b
+const divisao = d / a
+const modulo = a % 2
+console.log(soma, subtracao, multiplicao, divisao, modulo)
+
+
+
+
+/*
+
+EXEMPLOS DE OPERAÇÕES: 
+
+( d++ ) PostFixed, que vem depois do operando.
+( ++d ) PreFixado, que vem antes do operando
+( op1 + op2 ) Que fica entre os dois.
+( -a )  Deixa negativo, operador que opera em um único operando
+
+*/
--- Estudos_JS_Udemy/JavaScript/CursoJavaScript/Section 4/Exercicios-JS/Fundamentos/funcao1.js
+++ Estudos_JS_Udemy/JavaScript/CursoJavaScript/Section 4/Exercicios-JS/Fundamentos/funcao1.js
@@ -11,7 +11,7 @@ imprimirSoma();
 
 // Função com retorno
 
-function soma(a, b = 0){
+function soma(a, b = 1){
     return a + b;
 }
 
